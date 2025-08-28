7937a09b8be0d8c2ae335e432c5e6ca8296d4459
Argenis CS <carvajalsolache1@gmail.com>
2025-08-28 16:56:50 -0600

    Operacions Basicas

---
+++ codigo1/app.js
@@ -0,0 +1,7 @@
+let numero1 = parseFloat(prompt("Escribe el primer numero"));
+let numero2 = parseFloat(prompt("Escribe el segundo numero"));
+
+console.log("Suma " + (numero1 + numero2));
+console.log("Resta " + (numero1 - numero2));
+console.log("Multi " + (numero1 * numero2));
+console.log("Div " + (numero1 / numero2));
